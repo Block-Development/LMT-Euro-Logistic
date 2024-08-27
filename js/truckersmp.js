bdad0b91ad1e3b490c68e90f0ca9ed73af9d1116
InfoBlock <infoblock.official@gmail.com>
2024-08-27 16:21:36 +0200

    Add files via upload

--- js/truckersmp.js
+++ js/truckersmp.js
@@ -1,19 +1,20 @@
-const driversArea = document.querySelector('.drivers');
+const driversArea = document.querySelector('.drivers-area');
 const membersAPI = 'https://corsproxy.io/?' + encodeURIComponent('https://api.truckersmp.com/v2/vtc/73403/members')
 
 fetch(membersAPI)
     .then((response) => response.json())
     .then((json) => {
-        let drivers = json;
+        let drivers = json.response;
 
-        drivers.forEach((driver) => {
+        drivers.members.forEach((driver) => {
             if (driver.role_id == '207774') {
                 const div = document.createElement('div');
 
                 div.classList.add('lmt-driver');
 
-                driver.forEach((user) => {
-                    div.innerHTML += `<div class="col-sm-4">
+                let user = driver;
+
+                div.innerHTML += `<div class="col-sm-4">
                                         <div class="members">
                                           <div class="overlay"></div>
                                           <img class="img-responsive" src="https://lmt-euro-logistic.netlify.app/cdn/logo.png" alt="${user.username}">
@@ -22,9 +23,8 @@ fetch(membersAPI)
                                           <h3>${user.username}</h3>
                                         </div>
                                       </div>`;
-                });
 
-                document.querySelector('.drivers').appendChild(div);
+                document.querySelector('.drivers-area').appendChild(div);
             }
         });
     });
