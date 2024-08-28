909eaabab741930d8c783873682bd1418f3f2ecb
InfoBlock <infoblock.official@gmail.com>
2024-08-28 16:48:34 +0200

    Add files via upload

--- js/truckersmp.js
+++ js/truckersmp.js
@@ -1,5 +1,6 @@
 const membersAPI = 'https://corsproxy.io/?' + encodeURIComponent('https://api.truckersmp.com/v2/vtc/73403/members')
 const vtcAPI = 'https://corsproxy.io/?' + encodeURIComponent('https://api.truckersmp.com/v2/vtc/73403')
+const userAPI = 'https://corsproxy.io/?' + encodeURIComponent('https://api.truckersmp.com/v2/player/')
 
 const driversArea = document.querySelector('.drivers-area');
 const driverCount = document.querySelector('.driverCount');
@@ -10,7 +11,7 @@ fetch(vtcAPI)
         let driverCountRes = json.response;
 
         driverCount.innerHTML += `Unsere Firma besteht aus <strong>${driverCountRes.members_count}</strong> Fahrer*innen<br>Stand vom ${new Date().toLocaleDateString('de-DE')}`
-    })
+    });
 
 fetch(membersAPI)
     .then((response) => response.json())
@@ -23,17 +24,27 @@ fetch(membersAPI)
 
                 div.classList.add('lmt-driver');
 
-                div.innerHTML += `<div class="col-sm-4">
+                fetch(userAPI + driver.user_id)
+                  .then((response) => response.json())
+                  .then((json) => {
+                    let fetchAvatar = json.response.avatar;
+                    let driverAvatar = String(fetchAvatar)
+                      .replace('https://static.truckersmp.com/avatarsN/defaultavatar.png', 'https://lmt-euro-logistic.netlify.app/cdn/logo.png')
+                      .replace('https://static.truckersmp.com/avatarsN/4782497.1712159810.png', 'https://lmt-euro-logistic.netlify.app/cdn/logo.png')
+                      .replace('https://static.truckersmp.com/avatarsN/4192502.1713206807.png', 'https://lmt-euro-logistic.netlify.app/cdn/logo.png')
+
+                    div.innerHTML += `<div class="col-sm-4">
                                     <div class="members">
                                       <div class="members-img">
                                         <div class="overlay"></div>
-                                        <img class="img-responsive" src="https://lmt-euro-logistic.netlify.app/cdn/logo.png" alt="${driver.username}">
+                                        <img class="img-responsive" src="${driverAvatar}" alt="${driver.username}">
                                       </div>
                                       <div class="members-content">
-                                        <h3>${driver.username}</h3><span>${driver.joinDate}</span>
+                                        <h3><a href="https://truckersmp.com/user/${driver.user_id}" target="_blank">${driver.username}</a></h3><span>${driver.joinDate}</span>
                                       </div>
                                     </div>
                                   </div>`;
+                  })
 
                 document.querySelector('.drivers-area').appendChild(div);
             }
